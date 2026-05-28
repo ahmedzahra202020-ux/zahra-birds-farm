877bfb24eb8b1e63549ff6ae98c0e115c3359eb0
ahmedzahra202020-ux <ahmedzahra202020@gmail.com>
2026-05-28 03:36:07 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,35 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
 
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCJjoD16B9W97cdyipClDCkHqb1HC95-G4",
+  apiKey: "YOUR_API_KEY",
+
   authDomain: "zahra-birds-farm-f2481.firebaseapp.com",
+
   projectId: "zahra-birds-farm-f2481",
-  storageBucket: "zahra-birds-farm-f2481.firebasestorage.app",
-  messagingSenderId: "814463652536",
-  appId: "1:814463652536:web:649c8800d9a66076865a4d",
+
+  storageBucket: "zahra-birds-farm-f2481.appspot.com",
+
+  messagingSenderId: "YOUR_SENDER_ID",
+
+  appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence);
+
 export const db = getFirestore(app);
 
-export default app;
+export const storage = getStorage(app);
